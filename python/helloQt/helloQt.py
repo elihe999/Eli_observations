31cbf407bf38ca067232d0305c7cc61bc4d674f1
yili <eli.he999@hotmail.com>
2019-03-07 21:08:48 +0800

    <chore>Init qt project

---
+++ python/helloQt/helloQt.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+from PyQt5 import QtWidgets
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    w = QtWidgets.QWidget()
+    w.resize(400, 200)
+    w.setWindowTitle("hello PyQt5")
+    w.show()
+    exit(app.exec_())
